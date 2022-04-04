354660dfb4ec33253b569007f19c306a65b5d423
baroncurtin2 <baron.curtin@fast.co>
2022-04-04 17:11:14 -0400

    strings chapter complete

---
+++ server/README.md
@@ -0,0 +1,9 @@
+# Buiding a HTTP Server
+
+## Objective
+- Learning Rust > Server Performance
+
+## HTTP / 1.1
+- L7 Protocol
+- Sent over TCP
+- Message based
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,28 @@
+/*
+Strings
+- &str is a string slice to an immutable reference to a part of a String
+- Slicing strings with integers is not preferred
+- Rust strings are stored in UTF-8
+
+*/
+
 fn main() {
-    println!("Hello, world!");
+    let server = Server::new("127.0.0.1:8080".to_string());
+    server.run();
+}
+
+struct Server {
+    addr: String,
+}
+
+impl Server {
+    fn new(addr: String) -> Self {
+        Self {
+            addr,
+        }
+    }
+
+    fn run(self) {
+        println!("Listening on {addr}", addr=self.addr);
+    }
 }
