a519127b3fcf4b874343a44220a4b70128b7e94e
baroncurtin2 <baroncurtin2@users.noreply.github.com>
2022-04-05 01:08:50 -0400

    server tuples

--- server/src/main.rs
+++ server/src/main.rs
@@ -4,25 +4,27 @@ Strings
 - Slicing strings with integers is not preferred
 - Rust strings are stored in UTF-8
 
+Modules
+- All modules are private and all members are private
+
+Special Types
+- Option<T> is a type that can be either Some(T) or None
+- Result<T, E> is a type that can be either Ok(T) or Err(E)
 */
+use server::Server;
+use http::Request;
+use http::Method;
+
+mod server;
+mod http;
 
 fn main() {
     let server = Server::new("127.0.0.1:8080".to_string());
     server.run();
 }
 
-struct Server {
-    addr: String,
-}
-
-impl Server {
-    fn new(addr: String) -> Self {
-        Self {
-            addr,
-        }
-    }
-
-    fn run(self) {
-        println!("Listening on {addr}", addr=self.addr);
-    }
-}
+/*
+GET /user?id=10 HTTP/1.1\r\n
+HEADERS \r\n
+BODY
+*/
